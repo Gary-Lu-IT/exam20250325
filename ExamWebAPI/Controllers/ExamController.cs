b385b9a80b20f6fc9e15c31e93b9521f486220a5
Gary-Lu-IT <reimukaitoku@gmail.com>
2025-03-25 15:31:12 +0800

    更換專案，API可使用

---
+++ ExamWebAPI/Controllers/ExamController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamWebAPI.Controllers
+{
+    public class ExamController : Controller
+    {
+        /// <summary>
+        /// 判斷輸入的數字是否為質數 (測試API是否正常運作用的，不連接資料庫)
+        /// </summary>
+        /// <param name="InputNumber"></param>
+        /// <returns></returns>
+        [HttpPost][AllowAnonymous][Route("api/exam/isprime")]
+        public IActionResult IsPrime(int InputNumber)
+        {
+            if(InputNumber < 2)
+            {
+                return Ok(InputNumber.ToString()+"不是質數也不是合成數");
+            }
+            else
+            {
+                for (int i = 2; i < InputNumber; i++)
+                {
+                    if (InputNumber % i == 0)
+                    {
+                        return Ok(InputNumber.ToString() + "是合成數，找到2以上小於自身的因數："+i.ToString());
+                    }
+                }
+                return Ok(InputNumber.ToString() + "是質數");
+            }
+        }
+    }
+}
--- ExamWebAPI/Program.cs
+++ ExamWebAPI/Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "ExamWebAPI", Version = "v1" });
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -11,7 +15,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ExamWebAPI v1");
+        c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+    });
 }
 
 app.UseHttpsRedirection();
